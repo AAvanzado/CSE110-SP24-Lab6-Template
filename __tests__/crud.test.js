bdd5da8c0ea83ab2c2bab163be2bcf2d18840c2c
Angelo Avanzado <38778212+AAvanzado@users.noreply.github.com>
2024-05-15 18:30:14 -0700

    listing all crud tests to implement

--- __tests__/crud.test.js
+++ __tests__/crud.test.js
@@ -4,8 +4,34 @@ describe('Basic user flow for Website', () => {
     await page.goto('http://127.0.0.1:5500/index.html');
   });
 
-  // Next, check to make sure that all 20 <product-item> elements have loaded
-  it('template', async () => {
-    console.log('Checking for ...');
+  // Initial Note creation
+  it('Initial Note creation', async () => {
+    console.log('Initial Note creation...');
   });
+
+  // Editing and saving new note
+  it('Editing and saving new note', async () => {
+    console.log('Editing and saving new note...');
+  });
+
+  // Editing and saving previously made note
+  it('Editing and saving previously made note', async () => {
+    console.log('Editing and saving previously made note...');
+  });
+  
+  // Editing note then reloading while note is focused (should not save edits made)
+  it('Editing note then reloading while note is focused', async () => {
+    console.log('Editing note then reloading while note is focused...');
+  });
+  
+  // Unfocused note deletion
+  it('Unfocused note deletion', async () => {
+    console.log('Unfocused note deletion...');
+  });
+  
+  // checking all saved notes remain after reloading
+  it('checking all saved notes remain after reloading', async () => {
+    console.log('checking all saved notes remain after reloading...');
+  });
+  
 });
