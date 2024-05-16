9c2cf560d9875a884f9f860e95dbdd4650b86d0c
Angelo Avanzado <38778212+AAvanzado@users.noreply.github.com>
2024-05-15 17:57:22 -0700

    initial crud tester setup

--- __tests__/crud.test.js
+++ __tests__/crud.test.js
@@ -1,1 +1,11 @@
+describe('Basic user flow for Website', () => {
+  // First, visit the lab 8 website
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:5500/index.html');
+  });
 
+  // Next, check to make sure that all 20 <product-item> elements have loaded
+  it('template', async () => {
+    console.log('Checking for ...');
+  });
+});
